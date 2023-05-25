4f706d6612438d6baceb3d9755486ca7a9c09143
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-05-25 16:35:44 +0200

    Add cors and allowed origins

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import "./loadEnvironment.js";
 import createDebug from "debug";
 import app from "./server/index.js";
+import chalk from "chalk";
 
 export const debug = createDebug("happy-tails-api:root");
 
 const port = process.env.PORT ?? 4000;
 
 app.listen(port, () => {
-  debug(`Listening on http://localhost:${port}`);
+  debug(chalk.green(`Listening on http://localhost:${port}`));
 });
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,18 @@
+import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS;
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
 const app = express();
 
 app.disable("x-powered-by");
+
+app.use(cors(options));
 
 app.use(morgan("dev"));
 
