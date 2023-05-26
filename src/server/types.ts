fe79d891a3842b348d15835c6e1d20adbbb4587e
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-05-26 12:11:14 +0200

    Add loginUser controller

---
+++ src/server/types.ts
@@ -0,0 +1,12 @@
+import { type Request } from "express";
+
+export interface UserCredentialsStructure {
+  username: string;
+  password: string;
+}
+
+export type UserCredentialsRequest = Request<
+  Record<string, unknown>,
+  Record<string, unknown>,
+  UserCredentialsStructure
+>;
