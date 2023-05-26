4ec30e67a6503ad081231e5d0d1b81d0e3c8b68a
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-05-26 21:40:54 +0200

    Add userRouter test for invalid user credentials use case

--- src/server/routers/user/userRouter.test.ts
+++ src/server/routers/user/userRouter.test.ts
@@ -33,13 +33,18 @@ const mockUser: UserCredentialsStructure = {
   password: "mary",
 };
 
+const mockInvalidUser: UserCredentialsStructure = {
+  username: "Mar",
+  password: "mary",
+};
+
 const hashedMockUser: UserCredentialsStructure = {
   username: "Mary",
   password: "$2y$10$m1AiQlcchI32eXrtEl8ej.AiX9p1y/zD0OBEoVfDJoM7nwJf29gi.",
 };
 
 describe("Given a POST 'user/login' endpoint", () => {
-  describe("When it receives a request with a name 'Mary' and a password 'mary'", () => {
+  describe("When it receives a request with a username 'Mary' and a password 'mary'", () => {
     let newUser: UserDatabaseStructure;
 
     beforeAll(async () => {
@@ -49,16 +54,30 @@ describe("Given a POST 'user/login' endpoint", () => {
     test("Then it should respond with a status code 200 and a token", async () => {
       const expectedStatus = 200;
 
-      const response: { body: { token: string } } = await request(app)
+      const res: { body: { token: string } } = await request(app)
         .post(`${paths.user}${paths.login}`)
         .send(mockUser)
         .expect(expectedStatus);
 
-      const payload = jwt.verify(response.body.token, process.env.JWT_SECRET!);
+      const payload = jwt.verify(res.body.token, process.env.JWT_SECRET!);
 
       const userId = payload.sub as string;
 
       expect(userId).toBe(newUser._id.toString());
+    });
+  });
+
+  describe("When it receives a request with a invalid username 'Mar' and a password 'mary'", () => {
+    test("Then it should respond with a status code 401 and a message 'Wrong credentials'", async () => {
+      const expectedStatus = 401;
+      const expectedMessage = "Wrong credentials";
+
+      const res: { body: { message: string } } = await request(app)
+        .post(`${paths.user}${paths.login}`)
+        .send(mockInvalidUser)
+        .expect(expectedStatus);
+
+      expect(res.body.message).toBe(expectedMessage);
     });
   });
 });
