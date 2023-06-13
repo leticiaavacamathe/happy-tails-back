243db6ddc42fe572e87afffe968cede134e43a43
Leticia Avaca Mathe <leticia_a.m@hotmail.com>
2023-06-13 11:02:43 +0200

    Add getAnimal controller test for error case

--- src/server/controllers/animals/__tests__/getAnimal.test.ts
+++ src/server/controllers/animals/__tests__/getAnimal.test.ts
@@ -3,6 +3,7 @@ import Animal from "../../../../database/models/Animal";
 import { animalsMocks } from "../../../../mocks/animalMocks";
 import { getAnimal } from "../animalsControllers";
 import { type CustomRequest } from "../../../types";
+import CustomError from "../../../../CustomError/CustomError";
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -14,16 +15,17 @@ describe("Given a getAnimal controller", () => {
     status: jest.fn().mockReturnThis(),
     json: jest.fn(),
   };
-  describe("When it receives a request with the a valid id animal", () => {
-    const req: Partial<CustomRequest> = {
-      params: {
-        idAnimal: animalsMocks[0]._id.toString(),
-      },
-    };
+  const req: Partial<CustomRequest> = {
+    params: {
+      idAnimal: animalsMocks[0]._id.toString(),
+    },
+  };
 
+  describe("When it receives a request with the a valid id animal", () => {
     Animal.findById = jest.fn().mockReturnValue({
       exec: jest.fn().mockResolvedValue(animalsMocks[0]),
     });
+
     test("Then it should call the response status method with status code 200", async () => {
       const expectedStatus = 200;
 
@@ -46,6 +48,30 @@ describe("Given a getAnimal controller", () => {
       );
 
       expect(res.json).toHaveBeenCalledWith({ animal: excpectedAnimal });
+    });
+  });
+
+  describe("When it receives a request with a invalid aniaml id", () => {
+    test("Then it should call the next function with the error 404 and the message 'Animal not found'", async () => {
+      const expectedStatus = 404;
+      const expectedMessage = "Animal not found";
+
+      const expectedCustomError = new CustomError(
+        expectedStatus,
+        expectedMessage
+      );
+
+      Animal.findById = jest.fn().mockReturnValue({
+        exec: jest.fn().mockResolvedValue(null),
+      });
+
+      await getAnimal(
+        req as CustomRequest,
+        res as Response,
+        next as NextFunction
+      );
+
+      expect(next).toHaveBeenCalledWith(expectedCustomError);
     });
   });
 });
